1085f32cb0361317c9101a3a21fb3b12057b6ff1
Matthew Loewen <matthewlanimates@gmail.com>
2019-03-10 20:47:06 -0400

    Spawn Alife from Cube

---
+++ Synthetic-Captivatoin/Assets/Scripts/Spawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+
+    private float nextSpawnTime;
+
+    public GameObject Alife;
+    public float spawnDelay = 10;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (ShouldSpawn())
+        {
+            Spawn();
+        }
+    }
+
+    private void Spawn()
+    {
+        nextSpawnTime = Time.time + spawnDelay;
+        Instantiate(Alife, transform.position, transform.rotation);
+    }
+
+    private bool ShouldSpawn()
+    {
+        return Time.time >= nextSpawnTime;
+    }
+}
