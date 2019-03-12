1aefcda96f56c71b078c59522fde6da77663e7cb
Matthew Loewen <matthewlanimates@gmail.com>
2019-03-11 23:16:19 -0400

    adaptive needs
    Alife gets different resources as needed and then returns to nest to rest

--- Synthetic-Captivatoin/Assets/Scripts/ALife.cs
+++ Synthetic-Captivatoin/Assets/Scripts/ALife.cs
@@ -4,18 +4,37 @@ using UnityEngine;
 
 public class ALife : ALifeClass
 { 
+    //how quickly the Alife moves
     public float speed = 1.0f;
 
-    GameObject[] foodItem;
-    GameObject Selection;
 
     void Update()
     {
-        GameObject test = findCloseNeededResource("Food");
+        string need = live();
+
+        GameObject test = findCloseNeededResource(need);
 
         if (Vector3.Distance(test.transform.position,transform.position) > 13)
         {
             transform.position = Vector3.MoveTowards(transform.position, test.transform.position, speed * Time.deltaTime);
+        } else
+        {
+            switch (need)
+            {
+                case "Nest":
+                    rest = 100;
+                    break;
+                case "Food":
+                    hunger = 100;
+                    break;
+                case "Water":
+                    thirst = 100;
+                    break;
+                default:
+                    break;
+            }
         }
+
+   
     }
 }
--- Synthetic-Captivatoin/Assets/Scripts/ALifeClass.cs
+++ Synthetic-Captivatoin/Assets/Scripts/ALifeClass.cs
@@ -7,24 +7,72 @@ public class ALifeClass : MonoBehaviour
     //traits of the ALife
     public float hunger;
     public float thirst;
+    public float rest;
+    //enum LifeFactors
+    //{
+
+    //}
+
+    //current state of the Alife (what is it doing)
+    enum CurrentState {
+        eatting = 0,
+        drinking = 0,
+        resting = 0
+    };
+
+
 
     public void Start()
     {
         //stops all rotation through physics simulations
         GetComponent<Rigidbody>().freezeRotation = true;
+        //run lives costs every 2 seconds
+        InvokeRepeating("lifeCosts", 5, 1);
+
+        hunger = 50;
+        thirst = 50;
+        rest = 100;
     }
 
     //finite state machine here
     //this doesn't seem to be done right to me
-    public void live(float h, float t)
+    public string live()
     {
-        //thirst is more important than hunger is 
+        //there has to be a better way of doing this...
+        if (hunger < 90)
+        {
+            //go get water
+            return "Food";
+        }
+        else if (thirst < 80)
+        {
+            //go get food
+            return "Water";
+        }
+        else if (hunger < 80)
+        {
+            //go get food
+            return "Rest";
+        }
 
+        return "Nest";
     }
 
+
+    public void lifeCosts()
+    {
+        hunger--;
+        rest--;
+        thirst--;
+        Debug.Log(hunger + " " + thirst + " " + rest);
+    }
+
+
+    //PRE: String of Tag for a gameobject to find 
+    //POST: Returning game object with matching tag that is closest to play
     public GameObject findCloseNeededResource(string toFind)
     {
-        Debug.Log("searching for... " + toFind);
+        //Debug.Log("searching for... " + toFind);
         GameObject[] foundItem = GameObject.FindGameObjectsWithTag(toFind);
         if(foundItem.Length == 0)
         {
@@ -33,7 +81,6 @@ public class ALifeClass : MonoBehaviour
             return new GameObject();
         }else if(foundItem.Length == 1)
         {
-            Debug.Log("found 1 food");
             return foundItem[0];
         }
 
