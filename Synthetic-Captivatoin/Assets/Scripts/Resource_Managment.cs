9be10416cf14acf52b76762af4caf39b2b77e0bd
Matthew Loewen <matthewlanimates@gmail.com>
2019-04-08 15:25:37 -0400

    resources grow over time

--- Synthetic-Captivatoin/Assets/Scripts/Resource_Managment.cs
+++ Synthetic-Captivatoin/Assets/Scripts/Resource_Managment.cs
@@ -10,7 +10,7 @@ public class Resource_Managment : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("grow", 5, 5);
     }
 
     // Update is called once per frame
@@ -19,14 +19,30 @@ public class Resource_Managment : MonoBehaviour
         
     }
 
+    void grow()
+    {
+        if(checkSize())
+            transform.localScale += new Vector3(0.25f, 0.05f, 0.25f);
+    }
+
+    bool checkSize()
+    {
+        Vector3 size = GetComponent<Collider>().bounds.size;
+        if (size.x <= 2)
+        {
+            Destroy(gameObject);
+            return false;
+        } else if(size.x >= 4.5)
+        {
+            return true;
+        }
+        Debug.Log("THIS SHOULD NEVER BE REACHED");
+        return false;
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
         transform.localScale -= new Vector3(0.25f, 0.05f, 0.25f);
-        Vector3 size = GetComponent<Collider>().bounds.size;
-
-        if(size.x < 2)
-        {
-            Destroy(gameObject);
-        }
+        checkSize();
     }
 }
