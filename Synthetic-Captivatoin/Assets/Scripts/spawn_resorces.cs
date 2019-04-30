c7ad3d347d09315eae0518db9ef4b9b444ef5741
Matthew Loewen <matthewlanimates@gmail.com>
2019-04-30 15:19:51 -0400

    expermental spawn resources

---
+++ Synthetic-Captivatoin/Assets/Scripts/spawn_resorces.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawn_resorces : MonoBehaviour
+{
+
+    int preX = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("spawn", 2);
+    }
+
+    void spawn(){
+
+        int x = Random.Range(3,14) + (preX / 2);
+        
+
+
+
+        Invoke("spawn", x);
+    }
+
+}
