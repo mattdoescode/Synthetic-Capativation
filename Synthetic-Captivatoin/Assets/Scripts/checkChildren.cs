93c24f7973f2d0d41468d7fbe5b34249a0dfde54
Matthew Loewen <matthewlanimates@gmail.com>
2019-05-02 20:31:12 -0400

    destory nest if all its life is dead

---
+++ Synthetic-Captivatoin/Assets/Scripts/checkChildren.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class checkChildren : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("checkChildCount", 60, 20);
+    }
+
+    private void checkChildCount(){
+        if(transform.childCount <= 0){
+            Destroy(gameObject);
+        } 
+    }
+}
